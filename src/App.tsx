50b1bcf4c79bf753e126634d3155f931754332ca
Belom7 <Still07pkm@gmail.com>
2022-04-02 22:17:17 +0300

    01. Вложенные компоненты готово

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,17 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import {Header} from "./sait/Header";
+import {Body} from "./sait/Body";
+import {Footer} from "./sait/Footer";
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+    return (
+        <div className="App">
+            <Header title={'NEW HEADER'}/>
+            <Body title={'NEW BODY'}/>
+            <Footer title={'NEW FOOTER'}/>
+        </div>
+    );
 }
 
 export default App;
