39304e0f1e67526afcd0db146e8ea793937e857c
Belom7 <Still07pkm@gmail.com>
2022-04-03 12:17:43 +0300

    01. Вложенные компоненты готово

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,8 @@
 import React from 'react';
 import './App.css';
-import {Header} from "./sait/Header";
-import {Body} from "./sait/Body";
-import {Footer} from "./sait/Footer";
+import {Header} from "./01_props/Header";
+import {Body} from "./01_props/Body";
+import {Footer} from "./01_props/Footer";
 
 function App() {
     return (
